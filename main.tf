b8ae2a40a107c03b080fae6dc3221ac26a7897a2
juppad23 <juppad23@in.ibm.com>
2020-08-28 00:16:34 +0530

    Added null resource to check variable @

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 variable "ic_api_key"{}
-# variable "name"{}
+variable "name"{
+  type    = string
+  default = "somevalue"
+}
 provider "ibm"{
   ibmcloud_api_key=var.ic_api_key
 }
@@ -14,14 +17,23 @@ data "ibm_cis_domain" "web_domain" {
   cis_id = data.ibm_cis.web_instance.id
   domain = "cis-terraform.com"
 }
-# resource "ibm_cis_dns_record" "examplednsa" {
-#   cis_id           = data.ibm_cis.web_instance.id
-#   domain_id        = data.ibm_cis_domain.web_domain.id
-#   name      = var.name
-#   content   = "1.2.3.4"
-#   type      = "A"
-#   proxied = true
-# }
+resource "null_resource" "test_name" {
+  triggers = {
+    uuid = uuid()
+  }
+    
+  provisioner "local-exec" {
+    command = "echo - This val ${var.name}"
+  }
+}
+resource "ibm_cis_dns_record" "examplednsa" {
+  cis_id           = data.ibm_cis.web_instance.id
+  domain_id        = data.ibm_cis_domain.web_domain.id
+  name      = var.name
+  content   = "1.2.3.4"
+  type      = "A"
+  proxied = true
+}
 resource "ibm_cis_dns_record" "dns_mx_1" {
   cis_id           = data.ibm_cis.web_instance.id
   domain_id        = data.ibm_cis_domain.web_domain.id
